458a88d888552b9d79165941f7501cd7f8a3699e
Vanessa Fotso <vfotso@mitre.org>
2023-07-06 14:58:10 -0400

    migration
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 0) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_30_040058) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "document_references", force: :cascade do |t|
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
+
+  create_table "patient_sessions", force: :cascade do |t|
+    t.string "patient_key"
+    t.string "patient_id"
+    t.string "token"
+    t.string "token_expiration"
+    t.string "server_url"
+    t.string "auth_url"
+    t.datetime "created_at", precision: nil, null: false
+    t.datetime "updated_at", precision: nil, null: false
+  end
 
 end
