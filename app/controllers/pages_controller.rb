b76c80927dc1ff124648627c5eefe6995ace05c6
Vanessa Fotso <46642178+vanessuniq@users.noreply.github.com>
2023-10-04 23:57:40 -0400

    Add loader to advance directives pages for improved user feedback (#44)
    * Updated the patients view to be properly styled if patients_path is directly requested
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * added loader to advance directives page
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    * added loader to single advance directive page
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>
    
    ---------
    
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,23 @@
 
 # app/controllers/pages_controller.rb
 class PagesController < ApplicationController
-  before_action :delete_current_patient
-  before_action :require_server, only: %i[patients]
+  before_action :delete_current_patient, except: %i[patient_advance_directives advance_directive]
+  before_action :require_server, except: %i[fhir_servers]
+  before_action :retrieve_patient, only: %i[patient_advance_directives advance_directive]
 
   # GET /pages/patients
   def patients; end
 
   # GET /pages/fhir_servers
   def fhir_servers; end
+
+  # GET /pages/patients/:id/advance_directives
+  def patient_advance_directives; end
+
+  # GET /pages/advance_directives/:id
+  def advance_directive
+    @adi_id = params[:id]
+  end
 
   private
 
