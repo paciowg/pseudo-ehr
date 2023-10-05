23ca10e8b051f7d544fb5547cdbd0339d419bba9
Vanessa Fotso <vfotso@mitre.org>
2023-10-04 22:49:54 -0400

    added loader to advance directives page
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- config/routes.rb
+++ config/routes.rb
@@ -12,10 +12,13 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'pages/patients'
   get 'pages/fhir_servers'
+  get 'pages/patients/:id/advance_directives', to: 'pages#patient_advance_directives',
+                                               as: 'patient_advance_directives_page'
+  # GET /pages/patients/:id/advance_directives
   get '/login', to: 'sessions#login'
   post '/launch', to: 'sessions#launch_server'
   get '/disconnect', to: 'sessions#disconnect_server'
-  get 'patients/:patient_id/advance_directives', to: 'advance_directives#index'
+  get 'patients/:patient_id/advance_directives', to: 'advance_directives#index', as: 'patient_advance_directives'
   get 'advance_directives/:id', to: 'advance_directives#show'
   put 'advance_directives/:id', to: 'advance_directives#update_pmo'
   get 'advance_directives/:id/revoke', to: 'advance_directives#revoke_living_will'
