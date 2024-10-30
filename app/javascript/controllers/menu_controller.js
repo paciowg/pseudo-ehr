9697a37cd52fba650b5a2c57e37c06c4fb08aa1e
Vanessa Fotso <vfotso@mitre.org>
2024-10-30 17:32:07 -0400

    preventing fetch on hover over a link
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- app/javascript/controllers/menu_controller.js
+++ app/javascript/controllers/menu_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus"
+import { Turbo } from "@hotwired/turbo-rails";
 
 // Connects to data-controller="menu"
 export default class extends Controller {
@@ -27,5 +28,16 @@ export default class extends Controller {
 
     // Add active class to the clicked item
     event.currentTarget.classList.add("bg-blue-500", "text-white");
+  }
+
+  navigate(event) {
+    event.preventDefault();
+    const url = event.currentTarget.dataset.url;
+
+    // Navigate to the URL
+    if (url) {
+      // window.location.href = url;
+      Turbo.visit(url);
+    }
   }
 }
