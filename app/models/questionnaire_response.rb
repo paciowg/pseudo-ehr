961d98d4dd4ba80abec159688f2f6c44e9030417
Vanessa Fotso <vfotso@mitre.org>
2025-07-10 08:20:01 -0400

    fix parsing questionnaire identifier
    Signed-off-by: Vanessa Fotso <vfotso@mitre.org>

--- app/models/questionnaire_response.rb
+++ app/models/questionnaire_response.rb
@@ -52,16 +52,14 @@ class QuestionnaireResponse < Resource
 
   private
 
-  def parse_identifier(identifiers)
-    return [] if identifiers.blank?
+  def parse_identifier(identifier)
+    return if identifier.blank?
 
-    identifiers.map do |identifier|
-      {
-        system: identifier.system,
-        value: identifier.value,
-        type: identifier.type&.coding&.first&.display || identifier.type&.text
-      }
-    end
+    {
+      system: identifier.system,
+      value: identifier.value,
+      type: identifier.type&.coding&.first&.display || identifier.type&.text
+    }
   end
 
   def extract_questionnaire_title
