c2c1b0b5792ab5dfda3d8ee7ff0ac558ba575687
Vanessa Fotso <vfotso@mitre.org>
2025-08-08 00:12:59 -0400

    Refactor parameter validation in QuestionnaireResponseProcessingController for improved error handling

--- app/controllers/api/questionnaire_response_processing_controller.rb
+++ app/controllers/api/questionnaire_response_processing_controller.rb
@@ -3,17 +3,16 @@ module Api
   # into PFE FHIR Observations, then posting the results to an external FHIR server.
   class QuestionnaireResponseProcessingController < ApplicationController
     skip_before_action :current_server, raise: false
+    skip_before_action :verify_authenticity_token
+    before_action :validate_params, only: [:convert_qr_to_pfe_and_submit]
 
     def convert_qr_to_pfe_and_submit
-      if fhir_server_params.blank? || questionnaire_response_params.blank?
-        return render json: {
-          error: 'Missing required parameter(s): fhir_server, questionnaire_response'
-        }, status: :bad_request
-      end
+      fhir_server = params[:fhir_server]
+      qr_hash = questionnaire_response_params.to_h
 
       processor = QuestionnaireResponseProcessor.new(
-        questionnaire_response_params,
-        fhir_server: fhir_server_params
+        qr_hash,
+        fhir_server:
       )
 
       result = processor.call
@@ -31,8 +30,25 @@ module Api
       params.require(:questionnaire_response).permit!
     end
 
-    def fhir_server_params
-      params.require(:fhir_server).permit!
+    # Ensure the fhir_server param is present and is a url string
+    # and that the questionnaire_response param is present and is a hash and is a QuestionnaireResponse
+    def validate_params
+      if params[:fhir_server].blank? || !params[:fhir_server].is_a?(String)
+        render json: { error: 'Missing or invalid fhir_server parameter. Expecing a String URL' },
+               status: :bad_request and return
+      end
+
+      if questionnaire_response_params.blank? || !questionnaire_response_params.is_a?(ActionController::Parameters)
+        render json: {
+                 error: 'Missing or invalid questionnaire_response parameter.
+                 Expecting a FHIR QuestionnaireResponse resource'
+               },
+               status: :bad_request and return
+      end
+
+      return if questionnaire_response_params[:resourceType] == 'QuestionnaireResponse'
+
+      render json: { error: 'Invalid resourceType in questionnaire_response parameter' }, status: :bad_request
     end
   end
 end
