201d92f73a66225eb68d0453530b45d684c74437
Pete K <krautscheid@mitre.org>
2026-06-16 16:57:28 -0400

    Workaround for issue converting QuestionnaireResponses with contained resources

--- app/services/questionnaire_response_processor.rb
+++ app/services/questionnaire_response_processor.rb
@@ -11,7 +11,13 @@ class QuestionnaireResponseProcessor
     qr = build_questionnaire_response
 
     @client.begin_transaction
-    @client.add_transaction_request('PUT', "QuestionnaireResponse/#{qr.id}", qr)
+
+    # Historically pseudo-ehr has written the QuestionnaireResponse to the server along with the observations
+    # generated from the QuestionnaireResponse; because in the typical use case the QuestionnaireResponse is
+    # already present on the server we don't need to rewrite it, so we skip this step due to a bug in
+    # fhir_models when hydrating a QuestionnaireResponse with contained resources
+
+    # @client.add_transaction_request('PUT', "QuestionnaireResponse/#{qr.id}", qr)
 
     observations = PfeObservationBuilder.new(qr, questionnaire).build
 
