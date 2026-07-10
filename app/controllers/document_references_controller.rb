c86a0129e5282533a14a33498519fe398106232c
Pete K <krautscheid@mitre.org>
2026-07-09 22:47:59 -0400

    Get very basic DocumentReference Bundle support working

--- app/controllers/document_references_controller.rb
+++ app/controllers/document_references_controller.rb
@@ -1,7 +1,6 @@
 # app/controllers/document_references_controller.rb
 class DocumentReferencesController < ApplicationController
   before_action :require_server, :retrieve_patient, :set_resources_count
-  before_action :set_document_reference, only: [:bundle]
 
   # GET /patients/:patient_id/document_reference
   def index
@@ -15,23 +14,33 @@ class DocumentReferencesController < ApplicationController
 
   # GET /patients/:patient_id/document_references/:id/bundle
   def bundle
-    @bundle_content = @document_reference.bundle_contents.find { |content| content.id == params[:content_id] }
 
-    if @bundle_content.blank?
+    document_references = fetch_patient_document_reference(params[:patient_id]).values.flatten
+    document_reference = document_references.detect { |dr| dr.id == params[:id] }
+
+    if document_reference.blank?
+      raise "Document Reference #{params[:id]} not found"
+    end
+
+    # Just take the first contents entry for now
+    bundle_content = document_reference.bundle_contents.first
+
+    if bundle_content.blank?
       @bundle_error = 'Unable to load the document.'
       return
     end
 
-    matching_server = allowed_fhir_server_for(@bundle_content.url)
-
+    matching_server = FhirServer.all.find { |server| bundle_content.url.start_with?(server.base_url) }
+    
     if matching_server.blank?
       @bundle_error = 'Unable to load the document.'
       return
     end
 
     client = FhirClientService.new(fhir_server: matching_server).client
-    response = client.read(nil, nil, @bundle_content.url)
-    @bundle = response&.resource || response
+
+    # Make finding Bundle ID more robust
+    @bundle = client.read(FHIR::Bundle, bundle_content.url.split('/').last).resource
 
     unless @bundle.is_a?(FHIR::Bundle)
       @bundle_error = 'Unable to load the document.'
@@ -73,18 +82,6 @@ class DocumentReferencesController < ApplicationController
 
   def sort_and_group_docs(docs)
     docs.sort_by(&:full_date).reverse.group_by(&:identifier)
-  end
-
-  def set_document_reference
-    @document_reference = DocumentReference.find(params[:id])
-
-    return if @document_reference.present?
-
-    raise "Document Reference #{params[:id]} not found"
-  end
-
-  def allowed_fhir_server_for(url)
-    FhirServer.all.find { |server| url.start_with?(server.base_url) }
   end
 
   def extract_composition_title(bundle)
